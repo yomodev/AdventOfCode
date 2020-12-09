51446cda996dc63e35226f009a9ee18ff7317dcb
yomodev <fabiocongiu@gmail.com>
2020-12-10 00:33:24 +0100

    day 7 refactor

--- AdventOfCode2020CS/Day07.cs
+++ AdventOfCode2020CS/Day07.cs
@@ -6,84 +6,62 @@ namespace AdventOfCode2020CS
 {
     public class Day07
     {
+        static string SEED = "shiny gold";
+
         public static int Test1(string input)
         {
-            Dictionary<string, Dictionary<string, int>> bags = BuildBagDict(input);
-            HashSet<string> result = ShinyGoldContainers(bags);
-
-            return result.Count();
-        }
-
-        public static long Test2(string input)
-        {
-            Dictionary<string, Dictionary<string, int>> bags = BuildBagDict(input);
-            //HashSet<string> result = ShinyGoldContainers(bags);
-
-            var bag = new Bag("shiny gold");
-            var count = bag.Content(bags);
-            
-            return count -1;
-        }
-
-        public static HashSet<string> ShinyGoldContainers(Dictionary<string, Dictionary<string, int>> bags)
-        {
-            var stack = new Stack<string>(
-                            bags.Where(x => x.Value.ContainsKey("shiny gold"))
-                            .Select(x => x.Key)
-                            );
+            Dictionary<string, Dictionary<string, int>> dict = BuildDict(input);
 
             var result = new HashSet<string>();
+            
+            var stack = new Stack<string>(
+                dict.Where(x => x.Value.ContainsKey(SEED)).Select(x => x.Key));
+
             while (stack.Any())
             {
-                var check = stack.Pop();
-                if (!result.Contains(check))
+                var current = stack.Pop();
+                if (!result.Contains(current))
                 {
-                    result.Add(check);
-                    bags.Where(x => x.Value.ContainsKey(check))
+                    result.Add(current);
+                    dict.Where(x => x.Value.ContainsKey(current))
                         .Select(x => x.Key)
                         .ToList()
                         .ForEach(stack.Push);
                 }
             }
 
+            return result.Count();
+        }
+
+        public static long Test2(string input)
+        {
+            Dictionary<string, Dictionary<string, int>> dict = BuildDict(input);
+
+            long Count(string current) => 1 + dict[current].Sum(x => x.Value * Count(x.Key));
+            
+            var result = -1 + Count(SEED);
             return result;
         }
 
-        public static Dictionary<string, Dictionary<string, int>> BuildBagDict(string input)
+        public static Dictionary<string, Dictionary<string, int>> BuildDict(string input)
         {
-            var bags = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
-                            .Select(line => line.Split(" ,.".ToArray(), StringSplitOptions.RemoveEmptyEntries))
-                            .ToDictionary(words => $"{words[0]} {words[1]}", words =>
-                            {
-                                var d = new Dictionary<string, int>();
-                                if (!words.Contains("other"))
-                                {
-                                    for (int i = 4; i < words.Length; i += 4)
-                                    {
-                                        d.Add($"{words[i + 1]} {words[i + 2]}", int.Parse(words[i]));
-                                    }
-                                }
-                                return d;
-                            });
+            var dict = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                .Select(line => line.Split(" ,.".ToArray(), StringSplitOptions.RemoveEmptyEntries))
+                .ToDictionary(word => $"{word[0]} {word[1]}", word =>
+                {
+                    var content = new Dictionary<string, int>();
+                    if (!word.Contains("other"))
+                    {
+                        for (int i = 4; i < word.Length; i += 4)
+                        {
+                            content.Add($"{word[i + 1]} {word[i + 2]}", int.Parse(word[i]));
+                        }
+                    }
+                    return content;
+                });
 
-            return bags;
+            return dict;
         }
     }
 
-    internal class Bag
-    {
-        private string name;
-
-        public Bag(string v)
-        {
-            this.name = v;
-        }
-
-        internal long Content(Dictionary<string, Dictionary<string, int>> bags)
-        {
-            var cur = bags[name];
-            var n = cur.Sum(x => x.Value * new Bag(x.Key).Content(bags));
-            return 1 + n;
-        }
-    }
 }
