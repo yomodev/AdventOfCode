10b8282bc0449b4f6c4e30b6273427c7fff87203
yomodev <fabiocongiu@gmail.com>
2020-12-26 17:10:39 +0100

    refactor with extension method

--- AdventOfCode2020CS/Day14.cs
+++ AdventOfCode2020CS/Day14.cs
@@ -4,16 +4,9 @@ using System.Linq;
 
 namespace AdventOfCode2020CS
 {
-    public class Bin
+    public static class LongEx
     {
-        long value;
-
-        public Bin(long num = 0)
-        {
-            value = num;
-        }
-
-        public Bin Mask(string mask)
+        public static long Mask1(this long value, string mask)
         {
             var bs = Convert.ToString(value, 2);
             var a = mask.PadLeft(64, '0').ToArray();
@@ -25,13 +18,11 @@ namespace AdventOfCode2020CS
                     a[64 - len + i] = bs[i];
                 }
             }
-            return new Bin(Convert.ToInt64(string.Join(null, a).Replace('X', '0'), 2));
+
+            var str = string.Join(null, a).Replace('X', '0');
+            var result = Convert.ToInt64(str, 2);
+            return result;
         }
-
-        public static Bin operator +(Bin a, Bin b) => new Bin(a + b);
-
-        public static implicit operator long(Bin a) => a.value;
-
     }
 
     public class Day14
@@ -46,10 +37,10 @@ namespace AdventOfCode2020CS
                     data = x.Skip(1)
                         .Select(s => s.Split(new string[] { "mem[", "] = " }, StringSplitOptions.RemoveEmptyEntries))
                         .Where(s => s.Length > 0)
-                        .Select(n => new { address = int.Parse(n[0]), value = int.Parse(n[1]) }).ToArray()
+                        .Select(n => new { address = int.Parse(n[0]), value = long.Parse(n[1]) }).ToArray()
                 });
 
-            var dict = groups.SelectMany(x => x.data.Select(y => (k: y.address, v: new Bin(y.value).Mask(x.mask))))
+            var dict = groups.SelectMany(x => x.data.Select(y => (k: y.address, v: y.value.Mask1(x.mask))))
                 .GroupBy(k => k.k)
                 .ToDictionary(k => k.Key, v => v.Last());
 
