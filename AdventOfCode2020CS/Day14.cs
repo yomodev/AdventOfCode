e037410519aaf3255a140015896b2e5d8b811d46
yomodev <fabiocongiu@gmail.com>
2020-12-26 18:36:37 +0100

    day 14 part 2

--- AdventOfCode2020CS/Day14.cs
+++ AdventOfCode2020CS/Day14.cs
@@ -4,9 +4,9 @@ using System.Linq;
 
 namespace AdventOfCode2020CS
 {
-    public static class LongEx
+    public class Day14
     {
-        public static long Mask1(this long value, string mask)
+        public static long Mask(long value, string mask)
         {
             var bs = Convert.ToString(value, 2);
             var a = mask.PadLeft(64, '0').ToArray();
@@ -23,10 +23,7 @@ namespace AdventOfCode2020CS
             var result = Convert.ToInt64(str, 2);
             return result;
         }
-    }
 
-    public class Day14
-    {
         public static long Part1(string input)
         {
             var groups = input.Split("mask = ", StringSplitOptions.RemoveEmptyEntries)
@@ -40,11 +37,71 @@ namespace AdventOfCode2020CS
                         .Select(n => new { address = int.Parse(n[0]), value = long.Parse(n[1]) }).ToArray()
                 });
 
-            var dict = groups.SelectMany(x => x.data.Select(y => (k: y.address, v: y.value.Mask1(x.mask))))
+            var dict = groups.SelectMany(x => x.data.Select(y => (k: y.address, v: Mask(y.value, x.mask))))
                 .GroupBy(k => k.k)
                 .ToDictionary(k => k.Key, v => v.Last());
 
-            var result = dict.Sum(x=>x.Value.v);
+            var result = dict.Sum(x => x.Value.v);
+            return result;
+        }
+
+        public static string Mask2(int value, string mask)
+        {
+            var bs = Convert.ToString(value, 2);
+            var a = mask.PadLeft(64, '0').ToArray();
+            var len = bs.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if (a[64 - len + i] == '0')
+                {
+                    a[64 - len + i] = bs[i];
+                }
+            }
+
+            var result = string.Join(null, a)[(64 - 36)..];
+            return result;
+        }
+
+        public static IEnumerable<long> Address(string mask)
+        {
+            var offsets = mask.Select((x, i) => (x, i)).Where(x => x.x == 'X').Select(x => x.i).ToArray();
+            var range = (int)Math.Pow(2, offsets.Length);
+            foreach (var n in Enumerable.Range(0, range))
+            {
+                var b = Convert.ToString(n, 2).PadLeft(offsets.Length, '0');
+                var s = mask.ToArray();
+                var j = 0;
+                foreach (var i in offsets)
+                {
+                    s[i] = b[j++];
+                }
+
+                var result = Convert.ToInt64(string.Join(null, s), 2);
+                yield return result;
+            }
+        }
+
+        public static long Part2(string input)
+        {
+            var groups = input.Split("mask = ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Split(Environment.NewLine))
+                .Select(x => new
+                {
+                    mask = x.First(),
+                    data = x.Skip(1)
+                        .Select(s => s.Split(new string[] { "mem[", "] = " }, StringSplitOptions.RemoveEmptyEntries))
+                        .Where(s => s.Length > 0)
+                        .Select(n => new { address = int.Parse(n[0]), value = long.Parse(n[1]) }).ToArray()
+                });
+
+            var dict = new Dictionary<long, long>();
+            groups.SelectMany(x => x.data.Select(y => (value: y.value, mask: Mask2(y.address, x.mask))))
+                .ForEach(x =>
+                {
+                    Address(x.mask).ForEach(a => dict[a] = x.value);
+                });
+
+            var result = dict.Sum(x => x.Value);
             return result;
         }
 
--- TestAoC2020CS/UnitTest_day14.cs
+++ TestAoC2020CS/UnitTest_day14.cs
@@ -11,8 +11,7 @@ namespace TestAoC2020CS
         [TestMethod]
         public void Test_test1()
         {
-            var b1 = 11L;
-            var result = b1.Mask1("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
+            var result = Day14.Mask(11, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
             Assert.AreEqual(73, result);
         }
 
@@ -30,6 +29,36 @@ namespace TestAoC2020CS
             var input = File.ReadAllText(@"inputs\day14_1.txt");
             var result = Day14.Part1(input);
             Assert.AreEqual(18630548206046, result);
+        }
+
+        [TestMethod]
+        public void Test_test3()
+        {
+            var mask = Day14.Mask2(42, "000000000000000000000000000000X1001X");
+            Assert.AreEqual("000000000000000000000000000000X1101X", mask);
+
+            var values = Day14.Address(mask);
+            Assert.AreEqual(4, values.Count());
+            Assert.IsTrue(values.Contains(26));
+            Assert.IsTrue(values.Contains(27));
+            Assert.IsTrue(values.Contains(58));
+            Assert.IsTrue(values.Contains(59));
+        }
+
+        [TestMethod]
+        public void Test_test4()
+        {
+            var input = File.ReadAllText(@"inputs\day14_test2.txt");
+            var result = Day14.Part2(input);
+            Assert.AreEqual(208, result);
+        }
+
+        [TestMethod]
+        public void Test_part2()
+        {
+            var input = File.ReadAllText(@"inputs\day14_1.txt");
+            var result = Day14.Part2(input);
+            Assert.AreEqual(4254673508445, result);
         }
 
     }
