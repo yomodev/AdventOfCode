990b33dd5a12fcd1ecbe132efb3ebc61e774b3d0
yomodev <fabiocongiu@gmail.com>
2020-12-31 18:16:50 +0100

    day 16 part 1

---
+++ AdventOfCode2020CS/Day16.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020CS
+{
+    public class Day16
+    {
+
+        public static int Part1(string input)
+        {
+            var data = input.Split(Environment.NewLine + Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            var fields = data[0]
+                .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Split(new string[] { ": ", "-", " or " }, StringSplitOptions.RemoveEmptyEntries))
+                .Select(x => new { 
+                    name = x[0], 
+                    s1 = int.Parse(x[1]), 
+                    e1 = int.Parse(x[2]), 
+                    s2 = int.Parse(x[3]), 
+                    e2 = int.Parse(x[4])
+                });
+
+            var max = fields.Max(x => Math.Max(x.e1, x.e2));
+            var range = new bool[max +1];
+            fields.Select(x => Enumerable.Range(x.s1, x.e1 - x.s1 +1))
+                .SelectMany(x => x)
+                .Concat(fields.Select(x => Enumerable.Range(x.s2, x.e2 - x.s2 +1))
+                .SelectMany(x => x))
+                .ForEach(x => range[x] = true);
+
+            var nearby = data[2].Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .Select(x => x.Split(',').Select(n => int.Parse(n)).ToArray());
+
+            var result = nearby.SelectMany(x => x)
+                .Where(x => x > max || !range[x])
+                .Sum();
+
+            return result;
+        }
+
+        
+
+    }
+
+}
---
+++ TestAoC2020CS/UnitTest_day16.cs
@@ -0,0 +1,29 @@
+using AdventOfCode2020CS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+
+namespace TestAoC2020CS
+{
+    [TestClass]
+    public class UnitTest_day16
+    {
+        [TestMethod]
+        public void Test_test1()
+        {
+            var input = File.ReadAllText(@"inputs\day16_test1.txt");
+            var result = Day16.Part1(input);
+            Assert.AreEqual(71, result);
+        }
+
+        [TestMethod]
+        public void Test_part1()
+        {
+            var input = File.ReadAllText(@"inputs\day16_1.txt");
+            var result = Day16.Part1(input);
+            Assert.AreEqual(0, result);
+        }
+
+    }
+
+}
