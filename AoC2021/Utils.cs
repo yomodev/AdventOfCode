e5855aa77bf2fb469d0e58984df3d6ecfdb03e2c
Fabio Congiu <yomo@live.it>
2022-03-23 23:07:43 +0100

    IEnumerable<T> Buffer signature

--- AoC2021/Utils.cs
+++ AoC2021/Utils.cs
@@ -12,10 +12,12 @@ namespace AoC2021Lib
 		{
 			for (int i = start; i < end; i += step) yield return i;
 		}
+
 		public static IEnumerable<int> RangeEnd(int end, int step = 1)
 		{
 			for (int i = 0; i < end; i += step) yield return i;
 		}
+
 		public static IList<T> Fill<T>(this IList<T> list, int numberOfElements)
 		{
 			foreach (var _ in RangeEnd(numberOfElements)) { list.Add(default); }
@@ -28,11 +30,23 @@ namespace AoC2021Lib
 			var end = Math.Max(range.Start.Value, range.End.Value);
 			return Range(start, end);
 		}
+
 		public static IEnumerable<int> RangeInclusive(Range range)
 		{
 			var start = Math.Min(range.Start.Value, range.End.Value);
 			var end = Math.Max(range.Start.Value, range.End.Value);
-			return Range(start, end +1);
+			return Range(start, end + 1);
+		}
+
+		public static IEnumerable<T> Buffer<T>(this IEnumerable<T> source, int size, int overlap = 0)
+		{
+			if (size < 2) throw new ArgumentOutOfRangeException(nameof(size));
+			if (overlap < 0) throw new ArgumentOutOfRangeException(nameof(overlap));
+			if (source == null) throw new NullReferenceException(nameof(source));
+
+			List<T> list = new();
+			//list.Take
+			return list;
 		}
 	}
 }
