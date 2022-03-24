707c6c19be058e9ee0c53ec0c80d53c2aebc38a3
yomodev <fabiocongiu@gmail.com>
2022-03-24 23:24:00 +0100

    Break2

--- AoC2021/Utils.cs
+++ AoC2021/Utils.cs
@@ -7,46 +7,97 @@ using System.Threading.Tasks;
 namespace AoC2021Lib
 {
     public static class Utils
-	{
-		public static IEnumerable<int> Range(int start, int end, int step = 1)
-		{
-			for (int i = start; i < end; i += step) yield return i;
-		}
+    {
+        public static IEnumerable<int> Range(int start, int end, int step = 1)
+        {
+            for (int i = start; i < end; i += step) yield return i;
+        }
 
-		public static IEnumerable<int> RangeEnd(int end, int step = 1)
-		{
-			for (int i = 0; i < end; i += step) yield return i;
-		}
+        public static IEnumerable<int> RangeEnd(int end, int step = 1)
+        {
+            for (int i = 0; i < end; i += step) yield return i;
+        }
 
-		public static IList<T> Fill<T>(this IList<T> list, int numberOfElements)
-		{
-			foreach (var _ in RangeEnd(numberOfElements)) { list.Add(default); }
-			return list; // I chose this so that methods could be chained to it. 
-		}
+        public static IList<T> Fill<T>(this IList<T> list, int numberOfElements)
+        {
+            foreach (var _ in RangeEnd(numberOfElements)) { list.Add(default); }
+            return list; // I chose this so that methods could be chained to it. 
+        }
 
-		public static IEnumerable<int> Range(Range range)
-		{
-			var start = Math.Min(range.Start.Value, range.End.Value);
-			var end = Math.Max(range.Start.Value, range.End.Value);
-			return Range(start, end);
-		}
+        public static IEnumerable<int> Range(Range range)
+        {
+            var start = Math.Min(range.Start.Value, range.End.Value);
+            var end = Math.Max(range.Start.Value, range.End.Value);
+            return Range(start, end);
+        }
 
-		public static IEnumerable<int> RangeInclusive(Range range)
-		{
-			var start = Math.Min(range.Start.Value, range.End.Value);
-			var end = Math.Max(range.Start.Value, range.End.Value);
-			return Range(start, end + 1);
-		}
+        public static IEnumerable<int> RangeInclusive(Range range)
+        {
+            var start = Math.Min(range.Start.Value, range.End.Value);
+            var end = Math.Max(range.Start.Value, range.End.Value);
+            return Range(start, end + 1);
+        }
 
-		public static IEnumerable<T> Buffer<T>(this IEnumerable<T> source, int size, int overlap = 0)
-		{
-			if (size < 2) throw new ArgumentOutOfRangeException(nameof(size));
-			if (overlap < 0) throw new ArgumentOutOfRangeException(nameof(overlap));
-			if (source == null) throw new NullReferenceException(nameof(source));
+        public static IEnumerable<IEnumerable<T>> Buffer2<T>(this IEnumerable<T> collection, int size, int overlap = 0)
+        {
+            if (collection == null) throw new NullReferenceException(nameof(collection));
+            if (overlap < 0) throw new ArgumentOutOfRangeException(nameof(overlap));
+            if (size < 2) throw new ArgumentOutOfRangeException(nameof(size));
+            var removeFromQueue = 0;
 
-			List<T> list = new();
-			//list.Take
-			return list;
-		}
-	}
+            var queue = new Queue<T>();
+            foreach (var item in collection)
+            {
+                if (removeFromQueue > 0 && queue.Any())
+                {
+                    queue.Dequeue();
+                    removeFromQueue--;
+                }
+
+                queue.Enqueue(item);
+
+                if (queue.Count == size && removeFromQueue == 0)
+                {
+                    removeFromQueue = size - overlap;
+                    yield return queue.ToList();
+                }
+            }
+
+            if (queue.Count > 0 && removeFromQueue != size - overlap)
+            {
+                yield return queue.Skip(removeFromQueue).ToList();
+            }
+
+            yield break;
+        }
+
+        public static IEnumerable<IEnumerable<T>> Buffer<T>(this IEnumerable<T> collection, int size, int overlap = 0)
+        {
+            if (collection == null) throw new NullReferenceException(nameof(collection));
+            if (overlap < 0) throw new ArgumentOutOfRangeException(nameof(overlap));
+            if (size < 2) throw new ArgumentOutOfRangeException(nameof(size));
+
+            int counter = 0;
+            var queue = new Queue<T>();
+            var mod = 0;
+            foreach (var item in collection)
+            {
+                counter++;
+
+                if (queue.Count == size)
+                {
+                    queue.Dequeue();
+                }
+
+                queue.Enqueue(item);
+                mod = (counter - overlap) % size;
+                if (mod == 0 || counter == size)
+                {
+                    yield return queue.ToList();
+                }
+            }
+
+            yield return queue.Count == 0 ? null : queue.Skip(size - mod).ToList();
+        }
+    }
 }
--- TestAoC2021/TestUtils.cs
+++ TestAoC2021/TestUtils.cs
@@ -33,5 +33,53 @@ namespace TestAoC2021
             Assert.AreEqual(1, range.Count());
             Assert.AreEqual(3, range.Single());
         }
+
+        [TestMethod]
+        public void Buffer2_Test_Ov0()
+        {
+            var range = Utils.RangeInclusive(1..7);
+            var size = 3;
+            var windows = range.Buffer2(size).ToList();
+            Assert.AreEqual(3, windows.Count);
+            Assert.AreEqual(6, windows.First().Sum());
+            Assert.AreEqual(15, windows[1].Sum());
+            Assert.AreEqual(7, windows.Last().Single());
+        }
+
+        [TestMethod]
+        public void Buffer_Test_Ov0()
+        {
+            var range = Utils.RangeInclusive(1..7);
+            var size = 3;
+            var windows = range.Buffer(size).ToList();
+            Assert.AreEqual(3, windows.Count);
+            Assert.AreEqual(6, windows.First().Sum());
+            Assert.AreEqual(15, windows[1].Sum());
+            Assert.AreEqual(7, windows.Last().Single());
+        }
+
+        [TestMethod]
+        public void Buffer_Test_Ov1()
+        {
+            var range = Utils.RangeInclusive(1..7);
+            var size = 3;
+            var windows = range.Buffer(size, overlap: 1).ToList();
+            Assert.AreEqual(3, windows.Count);
+            Assert.AreEqual(6, windows.First().Sum());
+            Assert.AreEqual(12, windows[1].Sum());
+            Assert.AreEqual(18, windows.Last().Sum());
+        }
+
+        [TestMethod]
+        public void Buffer2_Test_Ov1()
+        {
+            var range = Utils.RangeInclusive(1..7);
+            var size = 3;
+            var windows = range.Buffer2(size, overlap: 1).ToList();
+            Assert.AreEqual(3, windows.Count);
+            Assert.AreEqual(6, windows.First().Sum());
+            Assert.AreEqual(12, windows[1].Sum());
+            Assert.AreEqual(18, windows.Last().Sum());
+        }
     }
 }
