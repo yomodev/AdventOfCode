c483a5517300878c217c03ef66a428f2a016861e
yomodev <yomo@live.it>
2025-12-03 02:51:01 +0100

    day1

---
+++ AoC2025/AoC2025/Day1.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Reflection.Emit;
+
+namespace AoC2025;
+
+public class Day1
+{
+    public static int Part1(string[] lines)
+    {
+        var pos = 50;
+        var size = 100;
+        var sum = 0;
+        foreach (var line in lines)
+        {
+            var dir = line[0];
+            var dist = int.Parse(line[1..]);
+            pos += dist * (dir == 'L' ? -1 : 1);
+            pos += pos >= size ? -size : 0;
+            pos %= size;
+            if (pos == 0)
+            {
+                sum++;
+            }
+        }
+
+        return sum;
+    }
+
+    public static int Part2(string[] lines)
+    {
+        var pos = 50;
+        var size = 100;
+        var sum = 0;
+
+        foreach (var line in lines)
+        {
+            var dir = line[0] == 'L' ? -1 : 1;
+            var (div, rem) = Math.DivRem(dir * int.Parse(line.AsSpan(1)), size);
+            var cur = pos + rem;
+            sum += Math.Abs(div);
+
+            if (cur <= 0)
+            {
+                if (pos != 0)
+                {
+                    sum++;
+                }
+
+                if (cur < 0)
+                {
+                    cur += size;
+                }
+            }
+            else if (cur >= size)
+            {
+                sum++;
+                cur -= size;
+            }
+
+            pos = cur;
+        }
+
+        return sum;
+    }
+}
