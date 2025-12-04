e49a3003f4df5414a1cadbfd05142e4935cc2bfd
yomodev <yomo@live.it>
2025-12-04 14:01:21 +0100

    day 4.1

---
+++ AoC2025/AoC2025/Day4.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Diagnostics;
+
+namespace AoC2025;
+
+public class Day4
+{
+    public static int Part1(IEnumerable<string> lines)
+    {
+        var matrix = lines.Select(line => line.ToCharArray()).ToArray();
+        int sum = 0;
+        int width = matrix[0].Length;
+        int height = matrix.Length;
+        for (int y = 0; y < matrix.Length; y++)
+        {
+            for (int x = 0; x < matrix[y].Length; x++)
+            {
+                var free = IsFree(matrix, x, y, width, height);
+                if (free) Debug.WriteLine($"Free at {x},{y}");
+                sum += free ? 1 : 0;
+            }
+        }
+
+        return sum;
+    }
+
+    private static bool IsFree(char[][] matrix, int x, int y, int width, int height)
+    {
+        var roll = '@';
+        if (matrix[y][x] == '.')
+        {
+            return false;
+        }
+
+        bool[] test =
+        {
+            x - 1 >= 0 && y - 1 >= 0 && matrix[y - 1][x - 1] == roll, // top-left
+            y - 1 >= 0 && matrix[y - 1][x] == roll, // top
+            x + 1 < width && y - 1 >= 0 && matrix[y - 1][x + 1] == roll, // top-right
+            x - 1 >= 0 && matrix[y][x - 1] == roll, // left
+            x + 1 < width && matrix[y][x + 1] == roll, // right
+            x - 1 >= 0  && y + 1 < height && matrix[y + 1][x - 1] == roll, // bottom-left
+            y + 1 < height && matrix[y + 1][x] == roll, // bottom-center
+            x + 1 < width && y + 1 < height && matrix[y + 1][x + 1] == roll, // bottom-right
+        };
+
+        var result = test.Count(x => x) < 4;
+        return result;
+    }
+
+    public static int Part2(IEnumerable<string> lines)
+    {
+        var sum = 0;
+        return sum;
+    }
+}
---
+++ AoC2025/AoC2025Tests/Day4Tests.cs
@@ -0,0 +1,28 @@
+﻿using AoC2025;
+using AwesomeAssertions;
+using Xunit.Abstractions;
+
+namespace AoC2025Tests;
+
+public class Day4Tests(ITestOutputHelper output)
+{
+    [Theory]
+    [InlineData("Day4.0.txt", 13)]
+    [InlineData("Day4.1.txt", 1467)]
+    public void Test1(string fileName, int solution)
+    {
+        var lines = File.ReadLines($"TestData/{fileName}");
+        var result = Day4.Part1(lines);
+        result.Should().Be(solution);
+    }
+
+    [Theory]
+    [InlineData("Day4.0.txt", 0)]
+    [InlineData("Day4.1.txt", 0)]
+    public void Test2(string fileName, int solution)
+    {
+        var lines = File.ReadLines($"TestData/{fileName}");
+        var result = Day4.Part2(lines);
+        result.Should().Be(solution);
+    }
+}
