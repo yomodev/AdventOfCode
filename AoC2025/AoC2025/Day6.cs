7f19b9cefdc5b20e607a716a0e99b63ef23b9479
yomodev <yomo@live.it>
2025-12-06 11:34:09 +0100

    day 6.1

---
+++ AoC2025/AoC2025/Day6.cs
@@ -0,0 +1,42 @@
+﻿
+namespace AoC2025;
+
+public class Day6
+{
+    public static long Part1(string[] lines)
+    {
+        var sum = 0L;
+        var ops = lines.Last().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        var data = lines
+            .Take(lines.Length - 1)
+            .Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray())
+            .ToArray();
+
+        var results = ops.Select(x => x == "+" ? 0L : 1L).ToArray();
+        for (int i = 0; i < ops.Length; i++)
+        {
+            var op = ops[i] == "+"
+                ? (Func<long, long, long>)((long a, long b) => a + b)
+                : (Func<long, long, long>)((long a, long b) => a * b);
+            foreach (var item in data)
+            {
+                results[i] = op(results[i], item[i]);
+            }
+        }
+
+        sum = results.Sum();
+        return sum;
+    }
+
+    public static long Part2(string[] lines)
+    {
+        var sum = 0L;
+        var ops = lines.Last().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        var data = lines.Take(lines.Length - 1).Select(x => x.ToCharArray()).ToArray();
+
+        
+        
+        return sum;
+    }
+}
---
+++ AoC2025/AoC2025Tests/Day6Tests.cs
@@ -0,0 +1,28 @@
+﻿using AoC2025;
+using AwesomeAssertions;
+using Xunit.Abstractions;
+
+namespace AoC2025Tests;
+
+public class Day6Tests(ITestOutputHelper output)
+{
+    [Theory]
+    [InlineData("Day6.0.txt", 4277556)]
+    [InlineData("Day6.1.txt", 4719804927602)]
+    public void Test1(string fileName, long solution)
+    {
+        var data = File.ReadAllLines($"TestData/{fileName}");
+        var result = Day6.Part1(data);
+        result.Should().Be(solution);
+    }
+
+    [Theory]
+    [InlineData("Day6.0.txt", 3263827)]
+    [InlineData("Day6.1.txt", 0)]
+    public void Test2(string fileName, long solution)
+    {
+        var data = File.ReadAllLines($"TestData/{fileName}");
+        var result = Day6.Part2(data);
+        result.Should().Be(solution);
+    }
+}
