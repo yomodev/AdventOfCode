f8711470bc99fe140173466b9123516a9b3e0b6a
yomodev <yomo@live.it>
2025-12-08 13:38:28 +0100

    day 8.1

--- AoC2025/AoC2025/Day08.cs
+++ AoC2025/AoC2025/Day08.cs
@@ -2,15 +2,59 @@
 
 public class Day08
 {
-    public static int Part1(string[] lines)
+    public static int Part1(IList<(int x, int y, int z)> lines, int connections)
     {
-        var sum = 0;
-        return sum;
+        var d = new List<(Point p1, Point p2, double distance)>(lines.Count * lines.Count);
+        var p = lines.ToDictionary(k => new Point(k.x, k.y, k.z), v => new HashSet<Point> { new(v.x, v.y, v.z) });
+        var total = 0;
+
+        for (int i = 0; i < lines.Count - 1; i++)
+        {
+            for (int j = i + 1; j < lines.Count; j++)
+            {
+                if (i == j) continue;
+                var p1 = new Point(lines[i].x, lines[i].y, lines[i].z);
+                var p2 = new Point(lines[j].x, lines[j].y, lines[j].z);
+                var distance = Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2) + Math.Pow(p2.Z - p1.Z, 2));
+                d.Add((p1, p2, distance));
+            }
+        }
+
+        d.Sort((a, b) => a.distance.CompareTo(b.distance));
+        for (int i = 0; i < connections; i++)
+        {
+            var (p1, p2, distance) = d[i];
+            var set = p[p1];
+
+            if (set.Contains(p2)) continue;
+            set.UnionWith(p[p2]);
+            foreach (var item in set.Where(x => x != p1))
+            {
+                p[item] = set;
+            }
+        }
+
+        var m = p.Values.Distinct().Select(x => x.Count).OrderByDescending(h => h).Take(3);
+        total = m.Aggregate(1, (a, b) => a * b);
+        return total;
     }
 
     public static int Part2(string[] lines)
     {
         var result = 0;
         return result;
+    }
+}
+
+public readonly record struct Point
+{
+    public int X { get; init; }
+    public int Y { get; init; }
+    public int Z { get; init; }
+    public Point(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
     }
 }
--- AoC2025/AoC2025Tests/Day08Tests.cs
+++ AoC2025/AoC2025Tests/Day08Tests.cs
@@ -7,12 +7,15 @@ namespace AoC2025Tests;
 public class Day08Tests(ITestOutputHelper output)
 {
     [Theory]
-    [InlineData("Day08.0.txt", 0)]
-    [InlineData("Day08.1.txt", 0)]
-    public void Test1(string fileName, int solution)
+    [InlineData("Day08.0.txt", 10, 40)]
+    [InlineData("Day08.1.txt", 1000, 57564)]
+    public void Test1(string fileName, int connections, int solution)
     {
-        var data = File.ReadAllLines($"TestData/{fileName}");
-        var result = Day08.Part1(data);
+        var data = File.ReadLines($"TestData/{fileName}")
+            .Select(x => x.Split(','))
+            .Select(n => (int.Parse(n[0]), int.Parse(n[1]), int.Parse(n[2])))
+            .ToList();
+        var result = Day08.Part1(data, connections);
         result.Should().Be(solution);
     }
 
